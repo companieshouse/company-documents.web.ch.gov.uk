28ff9c838a4f393ca4f1081e384409ab0d5d8c7e
amoriartyCH <amoriarty@companieshouse.gov.uk>
2020-06-29 09:58:57 +0100

    bump version of dependency and remove unused imports

--- src/test/java/uk/gov/companieshouse/web/companydocuments/controller/HealthCheckControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/web/companydocuments/controller/HealthCheckControllerIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
