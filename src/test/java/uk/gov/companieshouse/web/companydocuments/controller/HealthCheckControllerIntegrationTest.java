e06a5ed610de1c8597fda772224fb592420a89a9
amoriartyCH <amoriarty@companieshouse.gov.uk>
2020-06-29 10:33:11 +0100

    Remove unused imports

--- src/test/java/uk/gov/companieshouse/web/companydocuments/controller/HealthCheckControllerIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/web/companydocuments/controller/HealthCheckControllerIntegrationTest.java
@@ -14,7 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@ExtendWith({MockitoExtension.class})
+@ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HealthCheckControllerIntegrationTest {
 
