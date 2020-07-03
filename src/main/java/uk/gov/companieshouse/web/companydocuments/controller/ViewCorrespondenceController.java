a22cdd21898fc3117ef2afaf5bbf265c057b51c2
amoriartyCH <amoriarty@companieshouse.gov.uk>
2020-07-03 15:37:38 +0100

    Fixed button issue and removed unused import

--- src/main/java/uk/gov/companieshouse/web/companydocuments/controller/ViewCorrespondenceController.java
+++ src/main/java/uk/gov/companieshouse/web/companydocuments/controller/ViewCorrespondenceController.java
@@ -3,7 +3,6 @@ package uk.gov.companieshouse.web.companydocuments.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
