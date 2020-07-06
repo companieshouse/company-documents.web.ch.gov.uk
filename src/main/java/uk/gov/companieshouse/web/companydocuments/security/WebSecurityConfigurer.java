f6312fd3e436c996ff052b4c67d1e6ee185e3fbb
ch-tstrickland <tstrickland@companieshouse.gov.uk>
2020-07-06 15:45:46 +0100

    Add CompanyAuthFilter, HijackFilter Hijack Filter is added to all paths to prevent session hijacking. The CompanyAuthFilter forces authentication on any paths that match its  URL, which is govered by the config we use in WebSecurityConfigurere  where we add it.

--- src/main/java/uk/gov/companieshouse/web/companydocuments/security/WebSecurityConfigurer.java
+++ src/main/java/uk/gov/companieshouse/web/companydocuments/security/WebSecurityConfigurer.java
@@ -1,14 +1,33 @@
 package uk.gov.companieshouse.web.companydocuments.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import uk.gov.companieshouse.auth.filter.CompanyAuthFilter;
+import uk.gov.companieshouse.auth.filter.HijackFilter;
 import uk.gov.companieshouse.session.handler.SessionHandler;
 
 @EnableWebSecurity
+// Ignore Sonar: Utility classes should not have public constructors (java:S1118)
+@java.lang.SuppressWarnings("squid:S1118")
 public class WebSecurityConfigurer {
+
+    @Configuration
+    @Order(1)
+    public static class GovUkAccountsSecurityFilterConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            HttpSecurity matcher = http.antMatcher("/company/**");
+            matcher.addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class);
+            matcher.addFilterBefore(new CompanyAuthFilter(), BasicAuthenticationFilter.class);
+            matcher.addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class);
+        }
+    }
 
     @Configuration
     public static class WebSecurityFilterConfig extends WebSecurityConfigurerAdapter {
@@ -17,6 +36,7 @@ public class WebSecurityConfigurer {
         protected void configure(HttpSecurity http) throws Exception {
 
             http.addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class);
+            http.addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class);
         }
     }
 }
