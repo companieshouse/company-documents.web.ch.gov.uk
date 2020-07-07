160ff11bbb25035c6ef78f80c392090246d2c675
ch-tstrickland <tstrickland@companieshouse.gov.uk>
2020-07-07 13:58:09 +0100

    Add stub list documents page We need a page with a path including /company/companyNumber/... that would require authentication - this allows us to prove that the authentication changes work. The list documents page provides that path.

---
+++ src/main/java/uk/gov/companieshouse/web/companydocuments/controller/ListCompanyDocumentsController.java
@@ -0,0 +1,21 @@
+package uk.gov.companieshouse.web.companydocuments.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/company-documents/company/{companyNumber}/list-company-documents")
+public class ListCompanyDocumentsController extends BaseController {
+
+
+    @GetMapping
+    public String getListCompanyDocu() {
+        return getTemplateName();
+    }
+
+    @Override
+    protected String getTemplateName() {
+        return "listCompanyDocuments";
+    }
+}
