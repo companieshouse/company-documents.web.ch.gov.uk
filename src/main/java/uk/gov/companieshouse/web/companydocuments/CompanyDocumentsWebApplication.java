d7ad34e4c1bd974666005f28c3bde0e2350bd21e
mreis-ch <mreis@companieshouse.gov.uk>
2020-07-08 14:19:16 +0100

    Add the user header
    Add user header banner which contains links to user profile, filings, companies and signing out.
    
    Add SessionService.java and SessionServiceImpl.java to get session data
    Add UserDetailsInterceptor.java and add it to the CompanyDocumentsWebApplication.java
    Create UserBar.html to display links
    Add UserBar to BaseLayout.html
    
    AOP-53

--- src/main/java/uk/gov/companieshouse/web/companydocuments/CompanyDocumentsWebApplication.java
+++ src/main/java/uk/gov/companieshouse/web/companydocuments/CompanyDocumentsWebApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.web.companydocuments.interceptor.LoggingInterceptor;
+import uk.gov.companieshouse.web.companydocuments.interceptor.UserDetailsInterceptor;
 
 @SpringBootApplication
 public class CompanyDocumentsWebApplication implements WebMvcConfigurer {
@@ -12,10 +13,13 @@ public class CompanyDocumentsWebApplication implements WebMvcConfigurer {
     public static final String APPLICATION_NAMESPACE = "company-documents.web.ch.gov.uk";
 
     private final LoggingInterceptor loggingInterceptor;
+    private final UserDetailsInterceptor userDetailsInterceptor;
 
-    public CompanyDocumentsWebApplication(LoggingInterceptor loggingInterceptor) {
+    public CompanyDocumentsWebApplication(LoggingInterceptor loggingInterceptor,
+            UserDetailsInterceptor userDetailsInterceptor) {
 
         this.loggingInterceptor = loggingInterceptor;
+        this.userDetailsInterceptor = userDetailsInterceptor;
     }
 
     public static void main(String[] args) {
@@ -26,5 +30,6 @@ public class CompanyDocumentsWebApplication implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry.addInterceptor(loggingInterceptor);
+        registry.addInterceptor(userDetailsInterceptor);
     }
 }
---
+++ src/main/java/uk/gov/companieshouse/web/companydocuments/interceptor/UserDetailsInterceptor.java
@@ -0,0 +1,67 @@
+package uk.gov.companieshouse.web.companydocuments.interceptor;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import uk.gov.companieshouse.web.companydocuments.session.SessionService;
+
+@Component
+public class UserDetailsInterceptor extends HandlerInterceptorAdapter {
+
+    private static final String USER_EMAIL = "userEmail";
+
+    private static final String SIGN_IN_KEY = "signin_info";
+    private static final String USER_PROFILE_KEY = "user_profile";
+    private static final String EMAIL_KEY = "email";
+
+    @Autowired
+    SessionService sessionService;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+        if (modelAndView == null) {
+            return;
+        }
+
+        if ((isGetRequest(request) && !isResumeRequest(request))
+                || isPostRequestRedirect(request, modelAndView)) {
+
+            addUserDetailsToModelAndView(modelAndView);
+        }
+    }
+
+    private boolean isGetRequest(HttpServletRequest request) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.GET.toString());
+    }
+
+    private boolean isPostRequestRedirect(HttpServletRequest request, ModelAndView modelAndView) {
+        return request.getMethod().equalsIgnoreCase(HttpMethod.POST.toString()) &&
+                !modelAndView.getViewName().startsWith(UrlBasedViewResolver.REDIRECT_URL_PREFIX);
+    }
+
+    private boolean isResumeRequest(HttpServletRequest request) {
+        return request.getRequestURI().endsWith("/resume");
+    }
+
+    private void addUserDetailsToModelAndView(ModelAndView modelAndView) {
+        Map<String, Object> sessionData = sessionService.getSessionDataFromContext();
+        Map<String, Object> signInInfo = (Map<String, Object>) sessionData.get(SIGN_IN_KEY);
+        if (signInInfo != null) {
+            Map<String, Object> userProfile = (Map<String, Object>) signInInfo
+                    .get(USER_PROFILE_KEY);
+
+            if (userProfile != null) {
+                modelAndView.addObject(USER_EMAIL, userProfile.get(EMAIL_KEY));
+            }
+        }
+    }
+}
